9dc3236cd17aaf88854a1f6cd41f8ad54e664372
yijunxu <yijunx@umich.edu>
2021-11-10 15:09:41 +0000

    added word exceptions

--- app/exceptions/word.py
+++ app/exceptions/word.py
@@ -1,8 +1,12 @@
 class WordDoesNotExist(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
+    def __init__(self, word_id: str) -> None:
+        self.http_code = 404
+        self.message = f"Word {word_id} does not exist"
+        super().__init__(self.message)
 
 
 class WordAlreadyExist(Exception):
-    def __init__(self, *args: object) -> None:
-        super().__init__(*args)
+    def __init__(self, word_title: str) -> None:
+        self.http_code = 409
+        self.message = f"Word {word_title} already exists"
+        super().__init__(self.message)

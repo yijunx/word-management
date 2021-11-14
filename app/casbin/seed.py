62d4fa0e395823f7cfc162c18aad3e6d95b6302b
yijunxu <yijunx@umich.edu>
2021-11-14 16:49:28 +0000

    reformat

--- app/casbin/seed.py
+++ app/casbin/seed.py
@@ -10,14 +10,8 @@ def seed_or_get_admin_user() -> str:
     name = os.getenv("ADMIN_USER_NAME")
     email = os.getenv("ADMIN_USER_EMAIL")
     id = os.getenv("ADMIN_USER_ID")
-    actor=User(
-                id=id,
-                name=name,
-                email=email
-            )
+    actor = User(id=id, name=name, email=email)
 
     with get_db() as db:
-        userRepo.get_or_create(
-            db=db, actor=actor
-        )
+        userRepo.get_or_create(db=db, actor=actor)
     return actor.id

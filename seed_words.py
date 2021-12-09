5d2321041eb8cfaa85b8b8524c957572ffdae0c7
yijunxu <yijunx@umich.edu>
2021-12-09 15:15:26 +0000

    added some initial stuff

--- seed_words.py
+++ seed_words.py
@@ -3,12 +3,23 @@ from app.schemas.word import WordCreate
 from app.schemas.user import User
 
 INITIAL_WORDS = [
-
+    WordCreate(),
+    WordCreate(),
+    WordCreate(),
+    WordCreate(),
+    WordCreate(),
+    WordCreate(),
+    WordCreate(),
+    WordCreate(),
+    WordCreate(),
 ]
 ACTOR = User()
 def main():
-    WordService.create_word(item_create="lol", actor="lol")
-
+    for word in INITIAL_WORDS:
+        try:
+            WordService.create_word(item_create=word, actor=ACTOR)
+        except:
+            pass
 
 if __name__ == "__main__":
     main()
